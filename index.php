5c5b68b497f23369a74da9f0649b581a58345e59
Rob W <robpwatkins@gmail.com>
2020-03-21 13:58:10 -0500

    learnin it

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+$names = ['BertP', 'Rob', 'Brian'];
+$animals = ['kangaroo', 'rabbit', 'turtle', 'fox', 'kitty'];
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+  
+    header {
+      background: lightgray;
+      padding: 2em;
+      text-align: center;
+    }
+  
+  </style>
+</head>
+<body>
+  
+    <ul>
+      <?php foreach ($names as $name) : ?>
+        <li><?= $name; ?></li>
+      <?php endforeach; ?>
+      <?php foreach ($animals as $animal) : ?>
+        <li><?= $animal; ?></li>
+      <?php endforeach; ?>
+    </ul>
+
+</body>
+</html>
