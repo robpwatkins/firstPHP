afcc9a703402e96eb75becbde5182a816ba25f4e
Rob W <robpwatkins@gmail.com>
2020-03-22 15:55:43 -0500

    killin ittttt

--- functions.php
+++ functions.php
@@ -9,3 +9,21 @@ function dd($val) {
 function canEnter($age) {
   return $age > 21;
 }
+
+function connectToDb() 
+{
+    try {
+      return $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'booboo69');
+    } catch (PDOException $e) {
+      echo 'Nope: ' . $e->getMessage();
+    }
+}
+
+function fetchAllTasks($pdo)
+{
+  $statement = $pdo->prepare('select * from todos');
+  
+  $statement->execute();
+  
+  return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
--- index.php
+++ index.php
@@ -1,32 +1,9 @@
 <?php
+require 'functions.php';
+require 'Task.php';
 
-class Task {
-  public $description;
+$pdo = connectToDb();
 
-  public $completed = false;
-
-  public function  __construct($description)
-  {
-    $this->description = $description;
-  }
-
-  public function complete()
-  {
-    $this->completed = true;
-  }
-
-  public function isComplete()
-  {
-    return $this->completed;
-  }
-}
-
-$tasks = [
-  new Task('Go to the store'),
-  new Task('Steak the 69 nacho'),
-  new Task('Do a puzzler')
-];
-
-$tasks[0]->complete();
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
