0dda697434050bcbaea7280ac4cf7653cd841daa
Rob W <robpwatkins@gmail.com>
2020-03-23 14:27:39 -0500

    yes

--- views/index.view.php
+++ views/index.view.php
@@ -1,31 +1,16 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
-  
-    header {
-      background: lightgray;
-      padding: 2em;
-      text-align: center;
-    }
-  
-  </style>
-</head>
-<body>
-  <ul>
-    <?php foreach ($tasks as $task) : ?>
-      <li>
-        <?php if ($task->completed) : ?>
-          <strike><?= $task->description; ?></strike>
-        <?php else: ?>
-          <?= $task->description; ?>
-        <?php endif; ?>
-      </li>
-    <?php endforeach; ?>
-  </ul>
-      
+<?php require('partials/head.php'); ?>
 
-</body>
-</html>
+<h1>My Tasks</h1>
+<ul>
+  <?php foreach ($tasks as $task) : ?>
+    <li>
+      <?php if ($task->completed) : ?>
+        <strike><?= $task->description; ?></strike>
+      <?php else: ?>
+        <?= $task->description; ?>
+      <?php endif; ?>
+    </li>
+  <?php endforeach; ?>
+</ul>
+      
+<?php require('partials/footer.php'); ?>
